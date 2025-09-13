383003e25b7c6818f7bc933449e040139427d31c
Javier G <javiermazaba10@gmail.com>
2025-09-12 20:13:05 -0500

    add tilted card, glass icons, worked on home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Link, NavLink } from "react-router-dom";
 
 const linkBase =
-    "px-3 py-2 rounded-xl transition hover:bg-slate-100";
+    "px-3 py-2 rounded-xl transition hover:bg-slate-200";
 const active =
     "bg-slate-900 text-white hover:bg-slate-900";
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,14 @@ import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 import { SiReact, SiNextdotjs, SiTailwindcss, } from 'react-icons/si';
 import { DiGit, DiHtml5 } from "react-icons/di";
-import { SiMysql, SiPython } from "react-icons/si";
+import { SiMysql, SiPython, SiGithub } from "react-icons/si";
 import { BiLogoTypescript } from "react-icons/bi";
 import { RiJavascriptFill, RiSupabaseFill } from "react-icons/ri";
+import { FaLinkedin } from "react-icons/fa";
+import { LuMail } from "react-icons/lu";
 import LogoLoop from "../components/LogoLoop.tsx";
+import TiltedCard from "../components/TiltedCard.tsx";
+import GlassIcons from "../components/GlassIcons.tsx";
 
 const techLogos = [
     { node: <DiGit />, title: "Git", href: "https://tailwindcss.com" },
@@ -20,28 +24,84 @@ const techLogos = [
     { node: <SiTailwindcss />, title: "Tailwind CSS", href: "https://tailwindcss.com" },
 ];
 
+const github = [
+    { icon: <SiGithub size={100}/>, color: 'green', label: 'Github' },
+]
+const linkedin = [
+    { icon: <FaLinkedin size={100} />, color: 'blue', label: 'LinkedIn' },
+]
+const mail = [
+    { icon: <LuMail size={100} />, color: 'red', label: 'Contact' },
+]
+
 export default function Home() {
     return (
         <>
             <Helmet><title>Home — Javier Golpe Juarez</title></Helmet>
-            <section className="mt-[75px] flex flex-col items-start gap-6">
-                <h1 className="text-4xl font-extrabold">
-                    Hi, I’m Javier Golpe
-                </h1>
-                <p className="max-w-2xl text-lg text-slate-700">
-                    I build performant web & mobile apps with React, TypeScript, and Supabase.
-                </p>
-                <div className="flex gap-3">
-                    <Link to="/projects" className="rounded-xl bg-slate-900 px-4 py-2 text-white">
-                        View Projects
-                    </Link>
-                    <Link to="/contact" className="rounded-xl border px-4 py-2">
-                        Get in touch
-                    </Link>
+            <section className="mt-[75px] flex flex-row items-center gap-6 backdrop-blur-lg border-white/30 shadow-md mx-auto max-w-5xl border
+            justify-between px-4 py-3 rounded-2xl">
+
+                <div>
+                    <div>
+                        <h1 className="text-4xl font-extrabold">
+                            Building Clean & Modern Web Experiences
+                        </h1>
+                    </div>
+                    <div className="my-4 w-3/4 h-full text-left">
+                        <p className="max-w-2xl text-lg text-slate-700">
+                            I’m Javier Golpe Juarez, a software developer passionate about web apps, design,
+                            and solving real-world problems with code.
+                        </p>
+                    </div>
+                    <div className="flex gap-3">
+                        <Link to="/projects" className="rounded-xl bg-slate-900 px-4 py-2 text-white">
+                            View Projects
+                        </Link>
+                    </div>
+
+                    <div>
+                        <div className="flex flex-row gap-6">
+                            <Link to="https://github.com/JavierGolpeJ">
+                                <GlassIcons items={github} className="custom-class"/>
+                            </Link>
+                            <Link to="https://www.linkedin.com/in/javier-golpe-jr-62b83a341">
+                                <GlassIcons items={linkedin} className="custom-class"/>
+                            </Link>
+                            <Link to="/contact">
+                                <GlassIcons items={mail} className="custom-class"/>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
+
+                <div>
+                    <TiltedCard
+                        imageSrc="src/images/avatar.jpg"
+                        altText="My Profile Picture"
+                        captionText="My Profile Picture"
+                        containerHeight="400px"
+                        containerWidth="400px"
+                        imageHeight="300px"
+                        imageWidth="300px"
+                        rotateAmplitude={12}
+                        scaleOnHover={1.1}
+                        showMobileWarning={false}
+                        showTooltip={false}
+                        displayOverlayContent={true}
+                        overlayContent={
+                            <p className="tilted-card-demo-text text-white backdrop-blur-lg
+                            shadow-md m-4 max-w-5xl rounded-xl p-1">
+                                Javier Golpe Juarez
+                            </p>
+                        }
+                    />
+
+                </div>
+
             </section>
             <section>
-                <div style={{ height: '200px', position: 'relative', overflow: 'hidden'}}>
+                <div style={{ height: '60px', position: 'relative', overflow: 'hidden'}}
+                     className=" border rounded-xl mt-3 backdrop-blur-lg border-white/30 shadow-md">
                 <LogoLoop
                     logos={techLogos}
                     speed={60}
